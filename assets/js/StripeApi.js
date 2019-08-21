e760e2909a14d4fdfcf1184c459b603ae25e9762
Dolhen <mathieu.dolhen@gmail.com>
2019-08-21 11:16:32 +0200

    Improve : Payment flow with some AJAX

--- assets/js/StripeApi.js
+++ assets/js/StripeApi.js
@@ -1,17 +1,33 @@
 var stripe = Stripe('pk_test_W2lQqUZ0BHapP3fyYddX35DJ009ZFdfKJt');
 
+var paymentbutton = $("#OnlinePaymentButton");
 
-$(document).ready(function(){
-    $('#pay').on('click', function(event){
-        var ID = $('#pay').data("role");
+paymentbutton.on('click', function(){
 
-        stripe.redirectToCheckout({
-            // Make the id field from the Checkout Session creation API response
-            // available to this file, so you can provide it as parameter here
-            // instead of the {{CHECKOUT_SESSION_ID}} placeholder.
-            sessionId: ID
-        }).then(function (result) {
-            alert(result);
-        });
-    })
+    var productId = paymentbutton.data('product');
+
+    $.ajax({
+        url : '/pay/product/' + productId,
+        type: 'GET',
+        success : function(code_html, statut){
+            paymentbutton.replaceWith(code_html);
+
+            $('#pay').on('click', function(event){
+                var ID = $('#pay').data("role");
+
+                stripe.redirectToCheckout({
+                    // Make the id field from the Checkout Session creation API response
+                    // available to this file, so you can provide it as parameter here
+                    // instead of the {{CHECKOUT_SESSION_ID}} placeholder.
+                    sessionId: ID
+                }).then(function (result) {
+                    alert(result);
+                });
+            })
+        },
+        error : function(resultat, statut, erreur){
+            console.log(resultat);
+            console.log("Ajax error");
+        }
+    });
 });
